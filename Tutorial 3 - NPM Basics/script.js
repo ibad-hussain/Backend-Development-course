6c5b830905e85761c0fb9f31f521ff7f41fdf444
ibad-hussain <ibadhussain2212@gmail.com>
2025-07-01 15:59:18 +0500

    Added Tutorial 3

---
+++ Tutorial 3 - NPM Basics/script.js
@@ -0,0 +1,26 @@
+// npm understanding
+
+// installing and uninstalling packages
+    // install -> npm install packageName
+    // uninstall -> npm uninstall packageName
+    // install a particular version -> napm install packageName@version
+
+// understanding node_modules
+
+// dependencies and devdependencies
+
+// scripts
+    // default scripts -> start , test
+    // custom scripts
+
+// packages installed:
+    // one-liner-joke
+    // accessibility
+    // express
+    // nodemon
+    // chalk
+    // slugify@1.2.0
+
+
+
+console.log("hello world")
