2dc8d1c844b738539da9ba30d41ea32d97ae7417
ibad-hussain <ibadhussain2212@gmail.com>
2025-07-08 09:46:43 +0500

    Added Tutorial 10

---
+++ Tutorial 10 - Intro to Database, MongoDB & Mongoose/index.js
@@ -0,0 +1,28 @@
+// Data Storage
+
+// Database
+
+// Types of Database
+    // 1. SQL - Data stored in a systematic manner (tables)
+        // eg, MySQL
+    // 2. NoSQL - Scattered data
+        // eg, MongoDB
+
+// Types of Server
+    // Application Server - WHich holds backend logic
+        // eg, NodeJS server
+    // Database Server - Which hosts any database
+        // eg, MongoDB server
+
+// MongoDB terminologies
+    // Collections
+    // Documents
+    // Schemas
+    // Models
+
+// Code side
+    // CODE                             DATABASE
+
+    // mongoose.connect     --->        Database creation
+    // mongoose.model       --->        Collection creation
+    // CREATE               --->        Document creation
